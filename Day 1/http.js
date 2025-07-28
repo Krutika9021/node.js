9d9defc414c53bf7cf0cfb2e4f15aef2ff397c9d
Krutika9021 <krutikasalve24@gmail.com>
2025-07-29 02:13:28 +0530

    Initial commit

---
+++ Day 1/http.js
@@ -0,0 +1,9 @@
+// HTTP PROTOCOL
+
+const http = require('http');
+
+const server = http.createServer(function(req, res){
+    res.end("hello world!");
+})
+
+server.listen(3000);
---
+++ Day 1/script.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+//APPEND
+/*
+fs.appendFile("hey.txt", " heyy, I'm doing fine!", function(err) {
+    if(err) console.log(err);
+    else console.log("done");
+})
+
+//RENAME
+fs.rename("hey.txt", "hello.txt", function(err) {
+    if(err) console.log(err);
+    else console.log("done");
+}) 
+
+*/
+//COPYY
+/*
+fs.copyFile("hello.txt", "./copy/any.txt", function(err) {
+    if(err) console.log(err.messege);
+    else console.log("done");
+})
+//DELETE FILE
+fs.unlink("hello.txt", function(err) {
+    if(err) console.log(err);
+    else console.log(err);
+})
+
+//DELETES FILE, WITHOUT RECURSIVE WE CANT DELETE FILE
+fs.rmdir("./copy",{recursive: true}, function(err) {
+    if(err) console.log(err);
+    else console.log("removed");
+})
+
+
+//DELETES FOLDER/ DIRECTORY
+fs.rm("./copy",{recursive : true}, function(err){
+    if(err) console.log(err);
+    else console.log("remove");
+})
+
+*/
+// WRITE FILE
+/*
+fs.writeFile("hello.txt", "Heyyy", function(err){
+    if(err) console.log(err);
+    else console.log("done");
+})
+
+// READS FILE
+fs.readFile("./hello.txt", 'utf8',function(err, data){
+    if(err) console.log(err);
+    else console.log(data);
+})  */
+
+//
