ebeffa68f3778113eb96fb5b6e6fc4948a79f426
Krutika9021 <krutikasalve24@gmail.com>
2025-09-26 21:25:34 +0530

    added

--- miniproject1/app.js
+++ miniproject1/app.js
@@ -5,14 +5,40 @@ const postModel = require('./models/post');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const multer = require('multer');
+const crypto = require("crypto");
+const path = require("path");
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/images/uploads')
+    },
+    filename: function (req, file, cb) {
+        crypto.randomBytes(12, function (err, bytes){
+            const fn = bytes.toString("hex") + path.extname(file.originalname)
+            cb(null, fn)
+        })
+    }
+})
+
+const upload = multer({ storage: storage })
+
+
 app.get('/', (req, res) => {
     res.render("index");
+});
+
+app.get("/test", (req, res) => {
+    res.render("test");
+});
+
+app.post("/upload", upload.single("image"), (req, res) => {
+    console.log(req.file);
 });
 
 app.get('/login', (req, res) => {
@@ -63,7 +89,6 @@ app.post('/post', isLoggedIn, async (req, res) => {
     await user.save();
     res.redirect("/profile");
 });
-
 
 app.post('/register', async (req, res) => {
     try {
